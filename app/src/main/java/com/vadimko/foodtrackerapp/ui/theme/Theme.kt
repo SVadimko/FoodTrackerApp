62c13de7fe766b30ed7b206692d6ba2e1017a969
Vadimko <144110@gmail.com>
2022-12-26 22:18:19 +0300

    first commit

---
+++ app/src/main/java/com/vadimko/foodtrackerapp/ui/theme/Theme.kt
@@ -0,0 +1,88 @@
+package com.vadimko.foodtrackerapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.vadimko.foodtrackerapp.ui.theme.Typography
+
+/*private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    *//* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    *//*
+)
+
+@Composable
+fun FoodTrackerAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}*/
+
+private val DarkColorPalette = darkColors(
+    primary = BrightGreen,
+    primaryVariant = DarkGreen,
+    secondary = Orange,
+    background = MediumGray,
+    onBackground = TextWhite,
+    surface = LightGray,
+    onSurface = TextWhite,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+)
+
+private val LightColorPalette = lightColors(
+    primary = BrightGreen,
+    primaryVariant = DarkGreen,
+    secondary = Orange,
+    background = Color.White,
+    onBackground = DarkGray,
+    surface = Color.White,
+    onSurface = DarkGray,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+)
+
+@Composable
+fun FoodTrackerAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
